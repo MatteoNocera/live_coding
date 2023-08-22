b710af330ee35a64f17435649d787646a7593d73
MatteoNocera <matteo_nocera@tiscali.it>
2023-08-22 12:53:15 +0200

    22.8

---
+++ live_js/assets/js/main.js
@@ -0,0 +1,30 @@
+/* Anni: chiediamo all'utente la sua età e comunichiamo l'anno di nascita. */
+
+/* 
+
+-prompt
+-let userAge
+-consoleLog
+-document.getElementById
+
+*/
+
+// Chiedere età all'utente
+
+let userAge = prompt('type your age');
+
+// Recupero l'anno corrente con una variabile
+
+const currentYear = 2023;
+
+// Sottraggo all'anno corrente gli anni dell'utente
+
+const birthYear = currentYear - Number(userAge);
+
+// Stampo in console il risultato
+
+console.log('Ciao sei nato nel ' + birthYear);
+
+// Stampare il risultato in pagina
+
+const greetingsElement = document.getElementById('greetings')
