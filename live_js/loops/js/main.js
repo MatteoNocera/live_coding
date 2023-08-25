5d8b92766cb36d218fdfc1eef06c949b717b5779
MatteoNocera <matteo_nocera@tiscali.it>
2023-08-25 13:10:20 +0200

    loops 25.08

---
+++ live_js/loops/js/main.js
@@ -0,0 +1,24 @@
+/* stampare tutti i numeri da 0 a 1000 */
+
+/* for (let i = 0; i <= 1000; i++) {
+    const prova = document.getElementById('prova')
+    prova.insertAdjacentHTML('beforeend', `<li>${i}</li>`)
+    console.log(i);
+} */
+
+// stampo in console numeri da 1000 a 100
+
+/* let listElement = document.getElementById('list');
+
+for (let i = 1000; i >= 100; i--) {
+    console.log(i);
+
+    const markup = `<li>${i}</li>`;
+    
+    //element.innerHTML += markup;
+
+    const liElement = document.createElement('li');
+    liElement.append(i);
+    listElement.append(liElement);
+
+} */
